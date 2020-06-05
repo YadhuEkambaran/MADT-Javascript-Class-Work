ea9977b54e833cfa653c7770fff679d3836ea9e0
Yadhukrishnan E <yadhuekambaram1992@gmail.com>
2020-06-05 11:22:17 -0400

    Foor loop and while loop example

--- js/test1.js
+++ js/test1.js
@@ -1,7 +1,23 @@
-let sum = 0;
-for (let index = 1000; index >= 17; ) {
-  sum = sum + index;
-  index = index - 11;
+function forLoopExample() {
+  let sum = 0;
+  for (let index = 1000; index >= 17; ) {
+    sum = sum + index;
+    index = index - 11;
+  }
+
+  console.log("Sum is - " + sum);
 }
 
-console.log(sum);
+function whileLoopExample() {
+  let sum = 0;
+  let i = 0;
+  while (i < 10) {
+    sum = sum + i;
+    i = i + 1;
+  }
+
+  console.log("Sum is - " + sum);
+}
+
+forLoopExample();
+whileLoopExample();
