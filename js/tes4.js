e1c1f59a4bf2ab5c30cba823517b4b5c4e3f9413
Yadhukrishnan E <yadhuekambaram1992@gmail.com>
2020-06-11 16:19:49 -0400

    Day three and four

---
+++ js/tes4.js
@@ -0,0 +1,18 @@
+let obj = Object();
+obj.id = 1;
+obj.name = "Yadhu";
+
+let arr = Array();
+arr.push(2);
+arr.push(3);
+
+arr.unshift(1);
+console.log(arr);
+
+arr.pop();
+
+console.log(arr);
+
+arr.shift();
+
+console.log(arr);
